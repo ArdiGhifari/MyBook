a8c74e72f59fadc8182a0925b936f7900dff6369
ArdiGhifari <106002679+ArdiGhifari@users.noreply.github.com>
2024-05-31 22:22:09 +0700

    Create mybook.dart

---
+++ lib/screens/mybook.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyBook extends StatefulWidget {
+  const MyBook({super.key});
+
+  @override
+  State<MyBook> createState() => _MyBookState();
+}
+
+class _MyBookState extends State<MyBook> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
