c511bc73c4c0addd27c761d849532c40d21e6f5c
Skypros98 <130964227+Skypros98@users.noreply.github.com>
2024-05-31 20:09:55 +0700

    Update profile_screen.dart

---
+++ lib/screens/profile_screen.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:mybook/constant.dart';
+import 'package:mybook/yourbooks.dart';
+
+
+class ProfileScreen extends StatefulWidget {
+  ProfileScreen({super.key});
+
+  VoidCallback? callback;
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    // ignore: deprecated_member_use
+    return WillPopScope(
+      onWillPop: () async{
+        print('hello');
+        widget.callback!();
+        return true;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: true,
+          elevation: 0.0,
+          backgroundColor: Colors.indigo,
+        ),
+        backgroundColor: Colors.yellow,
+        body: SingleChildScrollView(
+          child: Column(
+
+            children: [
+              Container(
+                height: 300,
+                width: double.infinity,
+                padding: EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+                color: Colors.indigo,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(7.0),
+                      width: 120,
+                      height: 120,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          color: Colors.white,
+                          width: 2.0
+                        )
+                      ),
+                      child: CircleAvatar(
+                        backgroundColor: Colors.white,
+                        child: Center(
+                          child: Text(
+                            userinitial!,
+                            style: TextStyle(color: Colors.black,fontSize: 98, fontWeight: FontWeight.w600),
+                          ),
+                        ),
+
+                      ),
+                    ),
+                    SizedBox(height: 40,),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: Text(
+                        username!,
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white
+                        ),
+                      ),
+                      )
+                  ],
+                ),
+              ),
+              Align(
+                alignment: Alignment.topLeft,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+                  child: Text(
+                    'Your Books',
+                    style: HeadingTextstyle.copyWith(fontSize: 23),
+                  ),
+                ),
+              ),
+              Yourbooks()
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
