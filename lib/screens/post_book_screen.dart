3b0b4ad46c1282e7a2244788e1d56b661ce56254
ArdiGhifari <106002679+ArdiGhifari@users.noreply.github.com>
2024-05-31 21:24:50 +0700

    Add post_screen

---
+++ lib/screens/post_book_screen.dart
@@ -0,0 +1,454 @@
+
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dotted_border/dotted_border.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:mybook/loadingdialog.dart';
+
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:mybook/errordialog.dart';
+import 'package:mybook/sucessdialog.dart';
+
+class AdminScreen extends StatefulWidget {
+  const AdminScreen({super.key});
+
+  @override
+  State<AdminScreen> createState() => _AdminScreenState();
+}
+
+class _AdminScreenState extends State<AdminScreen> {
+  XFile? image;
+  bool? textscanning;
+  String textscanner="No image to show";
+  TextEditingController title=TextEditingController();
+  TextEditingController Description=TextEditingController();
+  TextEditingController Price=TextEditingController();
+  TextEditingController Pages=TextEditingController();
+  TextEditingController Author=TextEditingController();
+
+  bool? light1=false;
+
+  final ImagePicker _imagePicker = ImagePicker();
+  String selectedCategory = 'Select Category';
+  final _firestore=FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  double initialRating=0.0;
+
+  List<String> categories= ['select category','comedy','lifesty','sport','thriller','mystery'];
+
+  Future<void> checkData()async{
+
+    CollectionReference users=_firestore.collection("books");
+    if(image==null || title==null || Description==null || Price==null || Author==null || selectedCategory=='select category' || initialRating==0.0)
+    {
+      showDialog(
+        context: context, 
+        builder: (c){
+          return ErrorDialog(
+            message: "Please fill the information proparly",
+          );
+        }
+        );
+    }
+    else{
+      showDialog(
+        context: context, 
+        builder: (c){
+          return LoadingDialog(
+            message:"Saving Data"
+          );
+        }
+        );
+        String imageUrl = await uploadImageToFirebaseStorage();
+        try{
+          users.add(
+            {
+              'title': title.text,
+              'description':Description.text,
+              'price': Price.text,
+              'pages': Pages.text,
+              'author': Author.text,
+              'image_url': imageUrl,
+              'isPopular': light1,
+              'categories':selectedCategory,
+              'rating':initialRating,
+              'Bookmark':[]
+            }
+          ).whenComplete((){
+
+            setState(() {
+              image = null;
+              textscanning = null;
+              textscanner = "No image to show";
+              title.text = "";
+              Description.text ="";
+              Price.text="";
+              Pages.text="";
+              light1 = false;
+              Author.text = "";
+              selectedCategory = "select Category";
+              initialRating = 0.0;
+            });
+            Navigator.pop(context);
+            showDialog(
+              context: context, 
+              builder: (c){
+                return const SuccesDialog(
+                  message: "Data Update Succesfully",
+                );
+              }
+              );
+
+          });
+        }
+        catch(e){
+          Navigator.pop(context);
+          print(e);
+        }
+    }
+
+  }
+  void galleryImage()async{
+    try{
+      XFile? imagetemp = await _imagePicker.pickImage(source: ImageSource.gallery);
+
+      if(imagetemp != null){
+        setState(() {
+          textscanner='true';
+          image=imagetemp;
+        });
+      }
+    }
+
+    catch(e){
+
+      textscanner='false';
+      image=null;
+
+      textscanner="Error Occured, Please try again";
+      setState(() {
+        
+      });
+    }
+  }
+
+  Future<String> uploadImageToFirebaseStorage()async{
+    try{
+      File imageFile = File(image!.path);
+      String fileName = DateTime.now().millisecondsSinceEpoch.toString() + '.jpg';
+      Reference storageReferance = FirebaseStorage.instance.ref().child('image/$fileName');
+
+      final metadata = SettableMetadata(contentType: 'image/jpeg');
+      await storageReferance.putFile(imageFile, metadata);
+
+      String imageUrl = await storageReferance.getDownloadURL();
+      return imageUrl; 
+    }catch(e){
+      print('Error uploading image: $e');
+      return '';
+    }
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add New Books',style: TextStyle(color: Colors.white),),
+        backgroundColor: Colors.indigo,
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: ()async{
+              try{
+                await _auth.signOut();
+                Navigator.pop(context);
+                print("User signed out");
+              }
+              catch(e){
+                print("Error signing out");
+              }
+            },
+             icon: Icon(
+              Icons.logout,
+              size: 30,
+              color: Colors.white,
+              )),
+        ],
+      ),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 30,horizontal: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.indigo[50],
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: TextFormField(
+                  controller: title,
+                  keyboardType: TextInputType.text,
+                  onChanged: (value){
+                    setState(() {
+                      
+                    });
+                  },
+                  decoration: InputDecoration(
+                    hintText: 'Book title',
+                    hintStyle: GoogleFonts.inter(
+                      fontSize: 18,
+                      color: Colors.grey.shade700,
+                      fontWeight: FontWeight.w600,
+                    ),
+                    prefixIcon: Icon(Icons.book_outlined,size: 24,color: Colors.indigo),
+
+                  ),
+                ),
+              ),
+              SizedBox(height: 20,),
+              Container(
+
+                width: double.infinity,
+                height: 150,
+                decoration: BoxDecoration(
+                  color: Colors.indigo[50],
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: TextFormField(controller: Description,
+                keyboardType: TextInputType.multiline,
+                maxLength: null,
+                
+                textInputAction: TextInputAction.newline,
+                onChanged: (value){
+                  setState(() {
+                    
+                  });
+                },
+                decoration: InputDecoration(
+                  hintText: "Book Description",
+                  contentPadding: EdgeInsets.all(10),
+                  hintStyle: GoogleFonts.inter(
+                    fontSize: 18,
+                    color: Colors.grey.shade700,
+                    fontWeight: FontWeight.w600,
+
+                  ),
+                  border: InputBorder.none
+                ),
+                ),
+              ),
+              SizedBox(height: 30,),
+              Container(
+                width: double.infinity,
+
+                decoration: BoxDecoration(
+                  color: Colors.indigo[50],
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: TextFormField(
+                  controller: Author,
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                    hintText: 'Author Name',
+                    hintStyle: GoogleFonts.inter(
+                      fontSize: 18,
+                      color: Colors.grey.shade700,
+                      fontWeight: FontWeight.w600,
+                    ),
+                    prefixIcon: Icon(Icons.person,size: 24,color: Colors.indigo,),
+
+                  ),
+                ),
+              ),
+              SizedBox(height: 30,),
+              DottedBorder(
+
+                borderType: BorderType.RRect,
+                radius: Radius.circular(20),
+                padding: EdgeInsets.all(6),
+                child: GestureDetector(
+                  onTap: (){
+                    galleryImage();
+                  },
+                  child: Container(
+                    width: 250,
+                    height: 450,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.indigo[50],
+                    ),
+
+                    child: (textscanner==null) ?Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.upload_file,
+                            size: 80,
+                            color: Colors.black.withOpacity(0.6),
+                          ),
+                          SizedBox(height: 25,),
+                          Text(
+                            'tap to upload image',
+                            style: GoogleFonts.inter(
+                              fontSize: 19,
+                              fontWeight: FontWeight.bold
+                            ),
+                          )
+                        ],
+                      ),
+                    ):(textscanner==false)? Center(child: Text(textscanner,style: TextStyle(color: Colors.black87),),):Image.file(File(image!.path)),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20,),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text('popular',
+                  style: GoogleFonts.inter(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 21,
+                  ),
+                  ),
+                  SizedBox(width: 20,),
+                  Transform.scale(
+                    scale: 1.5,
+                    child: Switch(
+
+                      thumbIcon: (light1==true)?MaterialStateProperty.resolveWith((state) => Icon(Icons.check,color: Colors.black,)):MaterialStateProperty.resolveWith((states) => Icon(Icons.close,color: Colors.white)),
+                      value: light1!,
+                      activeTrackColor: Colors.green,
+                      inactiveThumbColor: Colors.indigo,
+                      onChanged: (bool value){
+                        setState(() {
+                          light1=value;
+                        });
+                      },
+                    ),
+                    )
+                ],
+              ),
+              SizedBox(height: 30,),
+              Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: Row(
+                  children: [
+                    Text(
+                      'Category',
+                      style: GoogleFonts.inter(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 21,
+                      ),
+                    ),
+                    SizedBox(width: 10,),
+                    Container(
+                      padding: EdgeInsets.all(4),
+
+                      decoration: BoxDecoration(
+                        color: Colors.indigo[50],
+                        borderRadius: BorderRadius.circular(12)
+                      ),
+                      child: DropdownButton<String>(
+                        value: selectedCategory,
+
+                        items: categories.map((String category){
+                          return DropdownMenuItem<String>(
+                            value: category,
+                            child: Text(category),
+                            );
+                        }).toList(),
+                        onChanged: (newvalue){
+                          setState(() {
+                            selectedCategory = newvalue!;
+                          }
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20,),
+              Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: Row(
+                  children: [
+                    Text(
+                      'Rating',
+                      style: GoogleFonts.inter(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 21,
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    RatingBar.builder(
+                      initialRating: initialRating,
+                      minRating: 1,
+                      maxRating: 5,
+                      itemSize: 40.0,
+                      direction: Axis.horizontal,
+                      allowHalfRating: false,
+                      itemCount: 5,
+                      itemBuilder: (context, index) => Icon(
+                        Icons.star,
+                        color: Colors.amber,
+                      ),
+                      onRatingUpdate:(rating){
+                        setState(() {
+                          initialRating = rating;
+                        });
+                      }
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(height: 30,),
+              SizedBox(
+                width: double.infinity,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: (){
+                    checkData();
+                  },
+                  child: Text(
+                    'Upload Book',
+                    style: GoogleFonts.inter(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w700
+                    ),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.indigo,
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+                    ),
+                ),
+              ),
+            ],
+          ),
+
+        ),
+      ),
+    );
+  }
+}
