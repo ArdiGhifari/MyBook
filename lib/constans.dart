0c8969453a53972c73c87b5efe040fe9d6aa3b46
ArdiGhifari <106002679+ArdiGhifari@users.noreply.github.com>
2024-05-31 20:54:44 +0700

    Add some Dialog Pop up

---
+++ lib/constans.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+TextStyle HeadingTextstyle=GoogleFonts.poppins(
+  fontWeight: FontWeight.w600,
+  fontSize: 26
+);
